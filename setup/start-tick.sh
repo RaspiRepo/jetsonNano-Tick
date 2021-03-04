7069d6631e9e910a18a75f3133555ce0949ff47f
raspirepo <mariya.k@live.com>
2021-03-04 12:42:40 -0800

    custom port update

--- setup/start-tick.sh
+++ setup/start-tick.sh
@@ -5,7 +5,7 @@ echo "TICK stack start in progress...."
 sudo service influxdb  start
 sudo service telegraf  start
 sudo service kapacitor start
-sudo service chronograf start
+export PORT=8080 ; sudo service chronograf start
 
 echo "TICK stack started successfully...."
 echo "Checking service status..."
